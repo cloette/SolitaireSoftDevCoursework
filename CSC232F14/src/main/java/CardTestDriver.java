0a0cb558b9c88913847dd243caaba6efd7d08042
curib17 <connieuribe_2017@depauw.edu>
2014-09-26 19:41:09 -0400

    CardTestDriver JUnit test file

---
+++ CSC232F14/src/main/java/CardTestDriver.java
@@ -0,0 +1,19 @@
+/*
+@Author: Connie Uribe
+Date: 09/17/2014
+This file is to test the Card.java file
+*/
+public class CardTestDriver
+{
+       public static void main(String[] args)
+       {
+               Card card1 = new Card( "Spades", "Seven", "Face-up");
+               Card card2 = new Card( "Diamonds", "Jack", "Face-up");
+
+               System.out.println (card1);
+               System.out.println();
+               System.out.println (card2);
+       }
+}
+
+
